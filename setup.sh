9b369159dcd09f8cf8c620f89ddb9bbbb83da5fc
jarvis394 <tarnatovski@gmail.com>
2022-11-25 01:42:34 +0300

    Fix build commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
+yum install wget
+
+wget https://github.com/NixOS/patchelf/archive/refs/tags/0.17.0.tar.gz
+tar -xf 0.17.0.tar.gz
+cd patchelf-0.17.0
+./bootstrap.sh
+./configure
+make
+make install
+cd ..
+
 wget https://zlib.net/fossils/zlib-1.2.9.tar.gz
 tar -xf zlib-1.2.9.tar.gz
 cd zlib-1.2.9
 sh configure
 make
-ls -l libz*
-cp libz.so.1 ../node_modules/canvas/build/Release/
-pwd
-cd ../node_modules/canvas && npm install --build-from-source
+cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
+cd ..
+
+patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
